5a3a9ed5c7dc07cc14f23a67203c042ff7f15dfa
majockbim <majock.bim1@gmail.com>
2026-09-06 20:33:41 -0400

    feat(init): initialize hardware

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,45 @@
-#include "../include/main.hpp""
+#include "../include/main.hpp"
 
 int main() {
-    std::cout << "Hello, world!";
+    Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
 
-    return 0;
+    const int SERVO_MIN = 150, SERVO_MAX = 600;
+
+    /* HAL */
+    uint8_t readSwitches() {
+        // read discrete inputs from module in slot 1
+        return P1.readDiscrete(1);
+    }
+
+    void setServoAngle(uint8_t servoNum, int angle) {
+        // safety constraints
+        if (angle < 0) angle = 0;
+        if (angle > 180) angle = 180;
+
+        int pulseLength = map(angle, 0, 180, SERVO_MIN, SERVO_MAX);
+
+        pwm.setPWM(servoNum, 0, pulseLength);
+    }   
+
+    /* main lifecycle */
+    voide setup() {
+        Serial.begin(115200); // baud rate
+
+        Serial.println("initializing P1AM backplane");
+        while (!P1.init()) {
+            delay(100);
+        }
+        Serial.println("P1AM backplane initialized");
+
+        Serial.println("initializing PWM driver");
+        pwm.begin();
+
+        pwm.setPWNFreq(50); // 20ms period
+        
+        Serial.println("hardware initialized!");
+    }
+
+    void loop() {
+        // todo: state machine logic
+    }
 }
